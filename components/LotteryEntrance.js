fc6ba5c7cda45755ed380bc87e13bc2b56ccb3fd
Edgar Gutierrez <edgargutierrez130@gmail.com>
2023-05-23 09:10:11 -0700

    Added new div

--- components/LotteryEntrance.js
+++ components/LotteryEntrance.js
@@ -105,6 +105,7 @@ export default function LotteryEntrance() {
                     <div>Entrance Fee: {ethers.utils.formatUnits(entranceFee, "ether")}</div>
                     <div> ETH Number of Players: {numPlayers}</div>
                     <div>Recent Winner: {recentWinner}</div>
+                    <div>WHATS UPPPP</div>
                 </div>
             ) : (
                 <div>No Raffle Address Detected</div>
