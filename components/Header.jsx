c19ad446e1cb7505c27158bf76919d4d9a367c8f
Edgar Gutierrez <edgargutierrez130@gmail.com>
2023-05-23 08:28:41 -0700

    initial commit

--- components/Header.jsx
+++
@@ -1,9 +0,0 @@
-import { ConnectButton } from "web3uikit";
-
-export default function Header() {
-    return (<div>
-        Decentralized Lottery
-        <ConnectButton moralisAuth={false} />
-    </div>
-    )
-}
